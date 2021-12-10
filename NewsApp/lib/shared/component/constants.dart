aa1b28f64fc001a701a489a91609e5ee0b122ed7
Ahmed Esam <79063735+ahmedesam1369@users.noreply.github.com>
2021-12-10 08:50:11 +0200

    Delete NewsApp directory

--- NewsApp/lib/shared/component/constants.dart
+++
@@ -1,8 +0,0 @@
-
-// API used Links 
-// https://newsapi.org/v2/top-headlines?country=eg&category=business&apiKey=65f7f556ec76449fa7dc7c0069f040ca
-// https://newsapi.org/v2/top-headlines?country=eg&category=sports&apiKey=65f7f556ec76449fa7dc7c0069f040ca
-// https://newsapi.org/v2/top-headlines?country=eg&category=science&apiKey=65f7f556ec76449fa7dc7c0069f040ca
-
-// API Search
-// https://newsapi.org/v2/everything?country=eg&category=business&apiKey=65f7f556ec76449fa7dc7c0069f040ca
